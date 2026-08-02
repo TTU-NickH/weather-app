4b4305c9f5c02e2b8ba05bb136d92d096754fd49
TTU-NickH <nahogan09@gmail.com>
2026-08-01 21:49:58 -0500

    Add Flask weather API container

---
+++ api/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/weather")
+def weather():
+    return {
+        "city": "Murfreesboro",
+        "temperature" : 87,
+        "condition": "Sunny"
+    }
+
+app.run(host="0.0.0.0", port=5000)

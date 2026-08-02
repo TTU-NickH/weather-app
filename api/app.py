1eefe88df51da928efc8ed1360bb0deaa1bec463
TTU-NickH <nahogan09@gmail.com>
2026-08-02 12:31:22 -0500

    Integrate Flask backend with Open-Meteo API

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
+from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
@@ -7,10 +7,13 @@ app = Flask(__name__)
 
 @app.route("/weather")
 def weather():
-    return {
-        "city": "Murfreesboro",
-        "temperature" : 87,
-        "condition": "Sunny"
-    }
+    city = request.args.get("city", "Murfreesboro")
 
-app.run(host="0.0.0.0", port=5000)
+    response = requests.get(
+        "https://api.open-meteo.com/v1/forecast?latitude=35.85&longitude=-86.39&current=temperature_2m"
+    )
+
+    return response.json()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
